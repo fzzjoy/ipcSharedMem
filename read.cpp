1eb05cea672ee2a095d0d2056348c60304c5fb27
fzz <fzz>
2021-08-22 17:18:37 +0800

    添加信号量的同步机制，完善生产者消费者模型

--- read.cpp
+++ read.cpp
@@ -7,6 +7,7 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <error.h>
+#include <sys/sem.h>
 
 using namespace std;
 
@@ -14,7 +15,22 @@ int main()
 {
     int shm_id;
     int *share;
-    shm_id = shmget (0x123456, getpagesize(), IPC_CREAT | 0666);
+    int key = 0x123456;
+
+    int write_key = 0x111;
+    int read_key = 0x222;
+    int write_sem_id;
+    int read_sem_id;
+
+    // 创建并打开信号量
+    write_sem_id = semget(write_key, 1, IPC_CREAT | 0666);
+    read_sem_id = semget(read_key, 1, IPC_CREAT | 0666);
+
+    // 设置信号量初始值为0
+    semctl(write_sem_id, 0, SETVAL, 0);
+    semctl(read_sem_id, 0, SETVAL, 0);
+
+    shm_id = shmget (key, getpagesize(), IPC_CREAT | 0666);
     if(shm_id == -1)
     {
         perror("shmget()");
@@ -22,17 +38,48 @@ int main()
     }
     share = (int *)shmat(shm_id, 0, 0);
 
-    int cnt = 10;
+    int cnt = 5;
+    struct sembuf sops;
     while(cnt--)
     {
-        sleep(1);
+        // P（wait()）等待生产者完成生产
+        cout << "等待生产者完成生产" << endl;
+        sops.sem_num = 0;
+        sops.sem_op = -1; // -1
+        semop(write_sem_id, &sops, 1);
+        cout << "生产者已完成生产" << endl;
+
         printf("%d\n", *share);
+
+        // V（signal()）消费者完成消费 
+        sops.sem_num = 0;
+        sops.sem_op = 1; // +1
+        semop(read_sem_id, &sops, 1);
+
+        sleep(2);
     }
 
     // 删除对共享内存的映射 detach
     if(0 != shmdt(share))
     {
         cout << "detach share mem fail!" << endl;
+    }
+
+    // 删除共享内存
+    if(0 != shmctl(shm_id, IPC_RMID, nullptr))
+    {
+        perror("shmctl RMID fail!");
+    }
+
+    // 删除信号量
+    if(0 != semctl(write_sem_id, 0, IPC_RMID))
+    {
+        perror("semctl rm write_sem_id fail!");
+    }
+
+    if(0 != semctl(read_sem_id, 0, IPC_RMID))
+    {
+        perror("semctl rm read_sem_id fail!");
     }
     return 0;
 }
--- write.cpp
+++ write.cpp
@@ -7,20 +7,34 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <error.h>
+#include <sys/sem.h>
 
 using namespace std;
 
 int main()
 {
     int shm_id;
+    int write_sem_id;
+    int read_sem_id;
     int *share = nullptr;
     int num;
+    int key = 0x123456;
+    int write_key = 0x111;
+    int read_key = 0x222;
 
     srand(time(nullptr)); // 初始化随机数的发生器
-    cout << "page size: " << getpagesize() << endl;
+
+    // 创建并打开信号量
+    write_sem_id = semget(write_key, 1, IPC_CREAT | 0666);
+    read_sem_id = semget(read_key, 1, IPC_CREAT | 0666);
+
+    // 设置信号量初始值为0
+    semctl(write_sem_id, 0, SETVAL, 0);
+    semctl(read_sem_id, 0, SETVAL, 0);
 
     // 创建共享内存 需要指定读写权限
-    shm_id = shmget (0x123456, getpagesize(), IPC_CREAT | 0666);
+    cout << "page size: " << getpagesize() << endl;
+    shm_id = shmget (key, getpagesize(), IPC_CREAT | 0666);
     if(shm_id == -1)
     {
         perror("shmget error!");
@@ -30,12 +44,26 @@ int main()
     // 添加对共享内存的映射 attch
     share = (int *)shmat(shm_id, nullptr, 0);
     int cnt = 5;
+    struct sembuf v = {0, 1, SEM_UNDO};
+    struct sembuf p = {0, -1, SEM_UNDO};
     while(cnt--)
     {
         num = random() % 1000;
+
         *share = num;
         printf("write a random number %d\n", num);
-        sleep(1);
+
+        // V（signal()）生产者完成生产 
+        semop(write_sem_id, &v, 1);
+
+        // P（wait()）等待消费者消费
+        cout << "等待消费者完成消费" << endl;
+        auto ret = semop(read_sem_id, &p, 1);
+        if(ret != 0)
+        {
+            perror("semop read_sem_id error!");
+        }
+        cout << "消费者已完成消费" << endl;
     }
 
     // 删除对共享内存的映射 detach
